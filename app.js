3b95fb01e393e0c6ce573623b6b79f0c295a4e62
mangosax <mcguerty@att.net>
2023-09-29 07:20:04 -0500

    javascript variables

--- app.js
+++ app.js
@@ -5,6 +5,34 @@ console.log(
 
 // Exercise 1
 
+let lastName ;
+var age ;
+
+
 // Exercise 2
+const firstName = "Bart";
+lastName = "Simpson";
+age = 12;
+
 
 // Exercise 3
+let language = "JavaScript";
+let createdYear = "1995";
+let isCaseSensitive = true;
+
+
+//Exercise 4
+let price = 19.99;
+let isOnSale = false;
+let salePercentage = 15;
+let stock = 0;
+let inStock = false;
+let selectedSize = "M";
+
+
+//Exercise 5
+let title = "Name of the Wind";
+let author = "Patrick Rothfuss";
+let pageCount = 722;
+let bookmark = 456;
+let hasRead = true;
